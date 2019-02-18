87fb3c29e5af6e4ccff83837c635cdf45b53be7c
Aruna Sameera <asliyanage@yahoo.,com>
2019-02-19 00:10:12 +1300

    update comment

--- src/main/java/com/aruna/java8/methodRefernce/methodRefernceDemo.java
+++ src/main/java/com/aruna/java8/methodRefernce/methodRefernceDemo.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class methodRefernceDemo {
 
 	public static void main(String[] args) {
-		
+		//This is java 8 method Refernce
 		List<String> names=Arrays.asList("Aruna","Sameera","Liyanage","Shanika","Malalgoda");
 		//names.forEach(i -> System.out.println(i));
 		
