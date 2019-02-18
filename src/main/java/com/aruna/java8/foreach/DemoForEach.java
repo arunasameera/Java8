ecace7a951e8478a0399fc2fe1f033619f2eac69
Aruna Sameera <asliyanage@yahoo.,com>
2019-02-18 23:52:43 +1300

    update comment

--- src/main/java/com/aruna/java8/foreach/DemoForEach.java
+++ src/main/java/com/aruna/java8/foreach/DemoForEach.java
@@ -10,6 +10,7 @@ public class DemoForEach {
 
 	 public static void main( String[] args )
 	    {
+		 //This is java 8 foreach
 	       List<Integer> values=Arrays.asList(4,5,6,7,8);
 	       values.forEach(i -> System.out.println(i));
 	    }
