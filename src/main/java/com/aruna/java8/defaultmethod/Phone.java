bc8e3d4519ab589fa9daee29cbac870064063375
arunasameera <asliyanage@gmail.com>
2019-02-21 23:19:02 +1300

    Update Phone.java

--- src/main/java/com/aruna/java8/defaultmethod/Phone.java
+++ src/main/java/com/aruna/java8/defaultmethod/Phone.java
@@ -1,7 +1,7 @@
 package com.aruna.java8.defaultmethod;
 
 public interface Phone{
-	//This is interface for mobiles
+	//This is interface for mobiles for dev
 	void call();
 	default void message(){
 		System.out.println("Default method");
