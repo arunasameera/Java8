82f67708aec3062aaf746561d1c3b015ee46d689
Aruna Sameera <asliyanage@gmail.com>
2019-02-27 22:25:34 +1300

    update devops branch

--- src/main/java/com/aruna/java8/dateTime/DateTimeDemo.java
+++ src/main/java/com/aruna/java8/dateTime/DateTimeDemo.java
@@ -10,7 +10,7 @@ public class DateTimeDemo {
 
 	public static void main(String[] args) {
 		
-		//Date time in java8 using web application
+		//Date time in java8 using devops branch
 		
 		LocalDate d = LocalDate.now();
 		LocalDate birthday = LocalDate.of(1984, Month.AUGUST, 29);
